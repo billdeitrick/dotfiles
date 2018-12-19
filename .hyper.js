7456e06355816e94e95dcea5a17eb4fe23987fec
Bill Deitrick <bill@deitrick.us>
2018-12-19 17:11:14 -0500

    Dotfile updates for Windows and WSL compatibility

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"Hack Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Hack Nerd Font", Hack, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
