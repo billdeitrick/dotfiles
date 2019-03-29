e04a9f26c370042631a2fdb0d60f79476c39578e
Bill Deitrick <bill@deitrick.us>
2019-03-28 21:59:41 -0400

    Add posix-source function

---
+++ .config/fish/functions/posix-source.fish
@@ -0,0 +1,7 @@
+# Thanks to Michael Lewandowski: http://lewandowski.io/2016/10/fish-env/
+function posix-source
+	for i in (cat $argv)
+		set arr (echo $i |tr = \n)
+  		set -gx $arr[1] $arr[2]
+	end
+end
