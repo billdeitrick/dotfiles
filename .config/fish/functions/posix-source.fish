8b6652d99c284ade4616d837e28497fb85986ac6
Bill Deitrick <bill@deitrick.us>
2019-12-08 00:11:40 -0500

    Support comments and blank lines

--- .config/fish/functions/posix-source.fish
+++ .config/fish/functions/posix-source.fish
@@ -1,7 +1,9 @@
 # Thanks to Michael Lewandowski: http://lewandowski.io/2016/10/fish-env/
 function posix-source
 	for i in (cat $argv)
-		set arr (echo $i |tr = \n)
-  		set -gx $arr[1] $arr[2]
+		if string match --invert "#*" $i > /dev/null; and test -n $i
+			set arr (echo $i |tr = \n)
+			set -gx $arr[1] $arr[2]
+		end
 	end
 end
