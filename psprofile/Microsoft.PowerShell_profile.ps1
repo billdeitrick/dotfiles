95af1e28626c60affccb16c73a6761b916d6edd4
Bill Deitrick <bill@deitrick.us>
2019-12-22 17:08:20 -0500

    Add choco profile

--- psprofile/Microsoft.PowerShell_profile.ps1
+++ psprofile/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,8 @@ Get-ChildItem $functionsFolder | ForEach-Object {."$functionsFolder\$_"}
 
 Write-Host "$(U 0xe0b0) $(U 0xe0b0) $(U 0xe0b0) Your shell is loaded! $(U 0xe0b2) $(U 0xe0b2) $(U 0xe0b2)" -ForegroundColor Green
 Write-Host "For a list of custom commands, run 'Get-CustomFunctions'" -ForegroundColor Blue
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
