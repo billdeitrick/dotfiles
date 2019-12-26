374546ba24499c0c7fd698ab065c7cad139440f7
Bill Deitrick <bill@deitrick.us>
2019-12-26 13:17:06 -0500

    Fix symlinking on Windows with OneDrive KFM

--- scripts/symlink.ps1
+++ scripts/symlink.ps1
@@ -5,6 +5,15 @@
 # Get the current user profile path; this will be expanded when we pass off to an elevated shell below.
 $USERPROFILE = $env:USERPROFILE
 
+# Get the current OneDrive path; we assume KFM is used if OneDrive path is defined
+# String escaping here is a little wonky; this is necessary because with single ' powershell
+# won't expand the variables. But, we need to enclose the path in '' so it is parsed correctly.
+if ($env:OneDrive) {
+    $PSPROFILEPATH = "`'$env:ONEDRIVE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1`'"
+} else {
+    $PSPROFILEPATH = "$USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+}
+
 Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
     # Conemu
     Remove-Item -Path $USERPROFILE\AppData\Roaming\ConEmu.xml -Force
@@ -13,7 +22,7 @@ Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
     # VSCode
     Remove-Item -Path $USERPROFILE\AppData\Roaming\Code\User\settings.json -Force
     New-Item -Path $USERPROFILE\AppData\Roaming\Code\User\settings.json -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\VSCode\settings.json
-    
+
     # Git
     Remove-Item -Path $USERPROFILE\.gitconfig -Force
     New-Item -Path $USERPROFILE\.gitconfig -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\.gitconfig
@@ -22,8 +31,8 @@ Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
     New-Item -Path $USERPROFILE\.gitignore -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\.gitignore
 
     # PS Profile
-    Remove-Item -Path $USERPROFILE\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Force
-    New-Item -Path $USERPROFILE\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\psprofile\Microsoft.PowerShell_profile.ps1
+    Remove-Item -Path $PSPROFILEPATH -Force
+    New-Item -Path $PSPROFILEPATH -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\psprofile\Microsoft.PowerShell_profile.ps1
 
     # For debug
     #pause
