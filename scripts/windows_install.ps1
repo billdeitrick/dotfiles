17c949296ec81b20f5bce5b60d3cfe2fe362f003
Bill Deitrick <bill@deitrick.us>
2020-09-10 14:02:08 -0400

    Update Windows install; remove unused apps

--- scripts/windows_install.ps1
+++ scripts/windows_install.ps1
@@ -22,10 +22,12 @@ choco install git -y --params "/GitOnlyOnPath /NoAutoCrlf /WindowsTerminal /NoSh
 choco install hackfont-windows -y
 choco install vscode -y
 choco install conemu -y
-choco install fontforge -y
 choco install docker-desktop -y
 choco install typora -y
-choco install ffmpeg -y
+
+# Unused desktop apps
+#choco install fontforge -y
+#choco install ffmpeg -y
 
 ################################################################
 # Install Desktop Software                                     #
@@ -34,36 +36,38 @@ choco install ffmpeg -y
 Write-Host "######### Install Desktop Software #########"
 
 choco install 7zip -y
-choco install audacity -y
-choco install audacity-lame -y
-choco install burp-suite-free-edition -y
-choco install cloudberryexplorer.amazons3 -y
-choco install conemu -y
-choco install dymo-connect -y
-choco install gimp -y
 choco install hxd -y
-choco install imgburn -y
-choco install inkscape -y
-choco install irfanview -y
-choco install jitsi -y
-choco install kdiff3 -y
-choco install keepass -y
 choco install firefox -y
 choco install nmap -y
 choco install notepadplusplus -y
-choco install obs-studio -y
 choco install orca -y
-choco install pingplotter -y
 choco install fiddler -y
 choco install python -y
-choco install slack -y
 choco install winscp -y
 choco install wireshark -y
 choco install xmlnotepad -y
 choco install putty -y
-choco install rufus -y
 choco install windirstat -y
-choco install vcxsrv -y
+
+# Unused desktop apps
+#choco install audacity -y
+#choco install audacity-lame -y
+#choco install burp-suite-free-edition -y
+#choco install cloudberryexplorer.amazons3 -y
+#choco install dymo-connect -y
+#choco install gimp -y
+#choco install imgburn -y
+#choco install inkscape -y
+#choco install irfanview -y
+#choco install jitsi -y
+#choco install kdiff3 -y
+#choco install keepass -y
+#choco install chrome -y
+#choco install obs-studio -y
+#choco install pingplotter -y
+#choco install slack -y
+#choco install rufus -y
+#choco install vcxsrv -y
 
 # Cleanup
 Remove-Item -Path 'C:\Users\Public\Desktop\ConEmu (x64).lnk' -Force
@@ -80,25 +84,3 @@ Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module MSOnline -Force
 Install-Module Azure -Force
 Install-Module ExchangeOnline -Force
-
-################################################################
-# Manual Install Applications                                  #
-################################################################
-
-Write-Host "######### The Following Apps Should Be Manually Installed #########"
-
-Write-Host "* Adobe Acrobat Customization Wizard"
-Write-Host "* Adobe Photoshop Elements 2019"
-Write-Host "* Adobe Premiere Elements 2019"
-Write-Host "* C2G A/V Controller Manager"
-Write-Host "* Canon Remote Operation Viewer"
-Write-Host "* Chanalyzer"
-Write-Host "* Exacqvision"
-Write-Host "* Eye P.A."
-Write-Host "* GAM"
-Write-Host "* Inssider"
-Write-Host "* MirrorOp"
-Write-Host "* Sketchup"
-Write-Host "* Tamograph"
-Write-Host "* RealVNC Viewer"
-Write-Host "* PowerBI"
