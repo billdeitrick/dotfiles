c178105f9c19b8456d88aac05458e2b40dd0e2fb
Bill Deitrick <bill.deitrick@mepush.com>
2021-01-05 22:47:41 -0500

    Add windows terminal settings

--- scripts/symlink.ps1
+++ scripts/symlink.ps1
@@ -16,6 +16,9 @@ if ($env:OneDrive) {
 
 Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
 
+    Remove-Item -Path $USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Force
+    New-Item -Path $USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\windowsterminal\settings.json
+
     Remove-Item -Path $USERPROFILE\.gitignore -Force
     New-Item -Path $USERPROFILE\.gitignore -ItemType SymbolicLink -Value $USERPROFILE\dev\dotfiles\.gitignore
 
