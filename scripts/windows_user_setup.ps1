adec100b660d10de455b1894fd1091d359c82f30
Bill Deitrick <bill.deitrick@mepush.com>
2021-01-05 22:59:53 -0500

    Remove Code extensions (syncing), switch to pipx

--- scripts/windows_user_setup.ps1
+++ scripts/windows_user_setup.ps1
@@ -12,49 +12,17 @@ $pythonVerPath = $($(python --version).Replace('.', '').Replace(' ', '').Substri
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $env:AppData + "\Python\$pythonVerPath\Scripts", [System.EnvironmentVariableTarget]::User)
 
 ################################################################
-# Install Code Extensions                                      #
-################################################################
-
-Write-Host "######### Install Code Extensions #########"
-
-# Extensions for all
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension hbenl.vscode-test-explorer
-code --install-extension KnisterPeter.vscode-github
-code --install-extension mikestead.dotenv
-code --install-extension mohsen1.prettify-json
-code --install-extension ms-azure-devops.azure-pipelines
-code --install-extension ms-azuretools.vscode-azurefunctions
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-dotnettools.csharp
-code --install-extension ms-dotnettools.vscode-dotnet-runtime
-code --install-extension ms-python.python
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-wsl
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.powershell-preview
-code --install-extension msazurermtools.azurerm-vscode-tools
-code --install-extension rebornix.ruby
-code --install-extension ryanluker.vscode-coverage-gutters
-code --install-extension samuelcolvin.jinjahtml
-code --install-extension skyapps.fish-vscode
-code --install-extension TylerLeonhardt.vscode-pester-test-adapter
-code --install-extension wingrunr21.vscode-ruby
-
-################################################################
 # Install Python Stuff                                         #
 ################################################################
 
 Write-Host "######### Install Python Stuff #########"
 
-pip install --user pipenv
-pip install --user cookiecutter
-pip install --user cruft
+pip install --user pipx
+pipx ensurepath 
+
+pipx install pipenv
+pipx install cookiecutter
+pipx install cruft
 
 ################################################################
 # Install Powershell Modules                                   #
