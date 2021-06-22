c3fab18c71e5500e891d8fa2e9c0d7f3dc57b334
Bill Deitrick <4174527+billdeitrick@users.noreply.github.com>
2021-06-21 22:15:25 -0400

    Improve up function in fish

--- .config/fish/functions/up.fish
+++ .config/fish/functions/up.fish
@@ -1,3 +1,20 @@
 function up
-    ping -c 1 8.8.8.8
+
+    argparse --name=up 'h/help' -- $argv
+    or return
+
+    if set -q _flag_help;
+        echo "Checks whether or not the Internet is reachable."
+        echo ""
+        echo "Named Arguments/Flags:"
+        echo "  -h | --help"
+        echo "    Show this help menu."
+        return
+    end
+
+    if ping -c 2 8.8.8.8 -W 200 | grep "bytes from" > /dev/null
+        echo "✅ Yes!"
+    else
+        echo "❌ No!"
+    end
 end
