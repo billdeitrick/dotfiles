190466edabcb990b2aa0905268ef7a45d43ed612
Bill Deitrick <4174527+billdeitrick@users.noreply.github.com>
2021-10-26 23:41:10 -0400

    Updates for Windows terminal

--- psprofile/Microsoft.PowerShell_profile.ps1
+++ psprofile/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 $functionsFolder = "$env:USERPROFILE\dev\dotfiles\psfunctions"
 Get-ChildItem $functionsFolder | ForEach-Object {."$functionsFolder\$_"}
 
-Write-Host "$(U 0xe0b0) $(U 0xe0b0) $(U 0xe0b0) Your shell is loaded! $(U 0xe0b2) $(U 0xe0b2) $(U 0xe0b2)" -ForegroundColor Green
+Write-Host "$(U 0x1F680) Your shell is loaded! $(U 0x1F680)" -ForegroundColor Green
 Write-Host "For a list of custom commands, run 'Get-CustomFunctions'" -ForegroundColor Blue
 
 # Chocolatey profile
