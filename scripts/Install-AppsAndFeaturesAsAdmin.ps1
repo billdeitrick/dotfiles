3892686a37fad7bc3f9b9196197de7401395630a
Bill Deitrick <4174527+billdeitrick@users.noreply.github.com>
2021-10-26 22:46:20 -0400

    Remove OneNote; winget not installing store apps

--- scripts/Install-AppsAndFeaturesAsAdmin.ps1
+++ scripts/Install-AppsAndFeaturesAsAdmin.ps1
@@ -81,9 +81,6 @@ Write-Host "######### Windows Package Manager Packages Install #########" -Foreg
 
 winget import --accept-package-agreements --accept-source-agreements -i (Join-Path -Path $PSScriptRoot -ChildPath "winget-$(if ($Personal) { "personal" } else { "work" }).json")
 
-# Handle packages that don't install well from JSON file
-winget install --exact --id "9WZDNCRFHVJL" --accept-package-agreements --accept-source-agreements # OneNote for Windows 10
-
 Write-Host "######### End Windows Package Manager Packages Install #########" -ForegroundColor Yellow
 
 #endregion
