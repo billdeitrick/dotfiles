1d5035041f6a51cc1b770fd142f98e9118668ad2
Bill Deitrick <bill.deitrick@bemaservices.com>
2025-10-19 12:10:49 -0400

    oh-my-posh updates

--- psprofile/Microsoft.PowerShell_profile.ps1
+++ psprofile/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,8 @@
 # Load scripts; dotsource everything from psfunctions directory
 $functionsFolder = "$env:USERPROFILE\dev\dotfiles\psfunctions"
-Get-ChildItem $functionsFolder | ForEach-Object {."$functionsFolder\$_"}
+Get-ChildItem $functionsFolder | ForEach-Object {. "$($_.FullName)"}
 
 Write-Host "$(U 0x1F680) Your shell is loaded! $(U 0x1F680)" -ForegroundColor Green
 Write-Host "For a list of custom commands, run 'Get-CustomFunctions'" -ForegroundColor Blue
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+oh-my-posh --init --shell pwsh --config "$($env:USERPROFILE)\dev\dotfiles\psprofile\posh_profile.omp.json" | Invoke-Expression
--- scripts/symlink.ps1
+++ scripts/symlink.ps1
@@ -5,9 +5,9 @@
 # Get the current user profile path; this will be expanded when we pass off to an elevated shell below.
 $USERPROFILE = $env:USERPROFILE
 
-# Get the PowerShell profile path; use [Environment] so we can account for OD KFM
+# Get the PowerShell profile path for both PS 5 and PS 7+; use [Environment] so we can account for OD KFM
 $PSPROFILEPATH = "$([Environment]::GetFolderPath("MyDocuments"))\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-
+$PSPROFILEPATH = "$([Environment]::GetFolderPath("MyDocuments"))\PowerShell\Microsoft.PowerShell_profile.ps1"
 
 Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
 
@@ -16,6 +16,10 @@ Start-Process powershell.exe -Verb Runas -ArgumentList "-Command & {
 
     Remove-Item -Path '$USERPROFILE\.gitignore' -Force
     New-Item -Path '$USERPROFILE\.gitignore' -ItemType SymbolicLink -Value '$USERPROFILE\dev\dotfiles\.gitignore'
+
+    # PS Profile
+    Remove-Item -Path '$PSPROFILEPATH' -Force
+    New-Item -Path '$PSPROFILEPATH' -ItemType SymbolicLink -Value '$USERPROFILE\dev\dotfiles\psprofile\Microsoft.PowerShell_profile.ps1'
 
     # PS Profile
     Remove-Item -Path '$PSPROFILEPATH' -Force
